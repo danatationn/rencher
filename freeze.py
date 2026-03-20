178a7318bb433732fc819aa260b3b6864c1d4456
dan <lemontasteicetea@proton.me>
2026-03-20 16:08:47 +0200

    works on windows

--- freeze.py
+++ freeze.py
@@ -1,32 +1,50 @@
 """
-    HEAVILY inspired by the Nicotine+ cxfreeze script:
-    https://github.com/nicotine-plus/nicotine-plus/blob/master/build-aux/windows/setup.py
+HEAVILY inspired by the Nicotine+ cxfreeze script:
+https://github.com/nicotine-plus/nicotine-plus/blob/master/build-aux/windows/setup.py
 """
 
 import os
 import platform
 import shutil
 import sys
+import sysconfig
 import tempfile
 from pathlib import Path
 
 from cx_Freeze import Executable, setup
 
 if platform.system() != 'Windows':
-    raise NotImplementedError("Freezing is only supported on Windows!")
+    raise NotImplementedError('Freezing is only supported on Windows!')
 
 USAGE_MSG = f'\n\nUsage:\n\t{Path(__file__).name} build_dir [dest_dir]'
-TYPELIB_PREFIXES = ['Gtk-4', 'Gio-', 'Gdk-4', 'GLib-', 'Gsk-', 'GObject-', 'GModule-', 'GdkPixbuf-',
-                    'Adw-', 'Graphene-', 'HarfBuzz-', 'Pango-', 'PangoCairo-', 'cairo-', 'freetype2-',
-                    'GdkWin32-4', 'Win32-']
-base_prefix = Path(sys.base_prefix)  # /usr
+TYPELIB_PREFIXES = [
+    'Gtk-4',
+    'Gio-',
+    'Gdk-4',
+    'GLib-',
+    'Gsk-',
+    'GObject-',
+    'GModule-',
+    'GdkPixbuf-',
+    'Adw-',
+    'Graphene-',
+    'HarfBuzz-',
+    'Pango-',
+    'PangoCairo-',
+    'cairo-',
+    'freetype2-',
+    'GdkWin32-4',
+    'Win32-',
+]
+base_prefix = Path(os.environ['MSYSTEM_PREFIX'])
+if not base_prefix:
+    raise ValueError('MSYSTEM_PREFIX environment variable is not set')
 lib_dir = base_prefix / 'bin'
 lib_ext = '.dll'
-gui = 'Win32GUI'
 
 ldd_path = shutil.which('ntldd')
 if not ldd_path:
-    raise FileNotFoundError('ldd was not found in path (not installed?)')
+    raise FileNotFoundError('ntldd was not found in path (not installed?)')
 
 pkgconf_path = shutil.which('pkg-config')
 if not pkgconf_path:
@@ -36,52 +54,49 @@ if not pkgconf_path:
 def freeze(argv: list[str]):
     include_files: list[tuple[Path, Path]] = []
     if len(argv) == 1:
-        print('ERROR: No build dir specified!'+USAGE_MSG)
+        print('ERROR: No build dir specified!' + USAGE_MSG)
         return
     elif not os.path.isdir(argv[1]):
-        print(f'ERROR: "{argv[1]}" is not a directory!'+USAGE_MSG)
+        print(f'ERROR: "{argv[1]}" is not a directory!' + USAGE_MSG)
         return
 
     rencher_module = Path(argv[1]) / f'lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages/'
     rencher_gres = Path(argv[1]) / 'share/rencher/com.github.danatationn.rencher.gresource'
     if not rencher_module.is_dir():
-        print(f'ERROR: "{argv[1]}" is not valid ("{rencher_module}" does not exist)'+USAGE_MSG)
+        print(f'ERROR: "{argv[1]}" is not valid ("{rencher_module}" does not exist)' + USAGE_MSG)
         return
 
     include_files.extend(find_files('*', rencher_module, 'lib', recursive=True))
     include_files.extend(find_files('*', rencher_gres, 'share'))
 
-    include_files.extend(find_files('gdbus.exe', lib_dir))
-    include_files.extend(find_files('gdbus.exe', lib_dir))
+    include_files.extend(find_files('gdbus.exe', lib_dir, 'lib/'))
 
     include_files.extend(find_files('*.conf', '/etc/fonts/', 'share/fonts/', True))
-    include_files.extend(find_files('libgtk-4*', lib_dir))
-    include_files.extend(find_files('libadwaita-1*', lib_dir))
+    include_files.extend(find_files('libgtk-4*', lib_dir, 'lib/'))
+    include_files.extend(find_files('libadwaita-1*', lib_dir, 'lib/'))
     include_files.extend(find_files('gschemas.compiled', base_prefix / 'share/glib-2.0/schemas/'))
     include_files.extend(find_files('pixbuf-loaders.cache', base_prefix / 'lib/gdk-pixbuf-2.0/2.10.0/'))
-    # include_files.extend(find_files('gdk-pixbuf-query-loaders', base_prefix / 'bin/', 'lib/'))
 
     for prefix in TYPELIB_PREFIXES:
         include_files.extend(find_files(f'{prefix}*.typelib', base_prefix / 'lib/girepository-1.0/'))
 
-    loaders_dir = base_prefix / '/lib/gdk-pixbuf-2.0/2.10.0/'
+    loaders_dir = base_prefix / 'lib/gdk-pixbuf-2.0/2.10.0/'
     pixbuf_path = loaders_dir / 'loaders.cache'
 
     tmp_dir = Path(tempfile.mkdtemp())
     tmp_pixbuf_path = tmp_dir / 'loaders.cache'
-    with open(tmp_pixbuf_path, 'w') as temp_f, \
-        open(pixbuf_path) as f:
-            data = f.read()
-            temp_f.write(data.replace('lib\\\\gdk-pixbuf-2.0\\\\2.10.0\\\\loaders', 'lib'))
+    with open(tmp_pixbuf_path, 'w') as temp_f, open(pixbuf_path) as f:
+        data = f.read()
+        temp_f.write(data.replace('lib\\\\gdk-pixbuf-2.0\\\\2.10.0\\\\loaders', 'lib'))
     include_files.extend(find_files('loaders.cache', tmp_dir, 'lib'))
     include_files.extend(find_files(f'*.{lib_ext}*', loaders_dir / 'loaders', 'lib'))
 
-
     build_dir = Path(argv[1], 'bin/rencher')
     if len(argv) > 2:
-        dest_dir = argv[2]
+        dest_dir = Path(argv[2])
     else:
         dest_dir = Path('build').absolute()
+    icon_path = Path(__file__).parent / 'data' / 'assets' / 'rencher-icon.ico'
     # setup() requires the first argument to be 'build'
     argv[1] = 'build'
 
@@ -103,19 +118,22 @@ def freeze(argv: list[str]):
         },
         executables=[
             Executable(
-                base=gui,
+                base='gui',
                 script=build_dir,
                 target_name='rencher',
-                # icon=icon_path,
+                icon=icon_path,
                 # copyright=rencher.__copyright__,
             ),
         ],
     )
     shutil.rmtree(tmp_dir)
-    dir_name = f'exe.{platform.system().lower()}-{platform.machine()}-{sys.version_info.major}.{sys.version_info.minor}'
-    print(f'Your frozen app is at "{dest_dir}/{dir_name}"')
+    dir_name = f'exe.{sysconfig.get_platform()}-{sysconfig.get_python_version()}'
+    print(f'Your frozen app is at "{dest_dir / dir_name}"')
 
-def find_files(pattern: str, search_path: Path | str, dest_path: Path | str = Path(), recursive: bool = False) -> list[tuple[Path, Path]]:
+
+def find_files(
+    pattern: str, search_path: Path | str, dest_path: Path | str = Path(), recursive: bool = False
+) -> list[tuple[Path, Path]]:
     source_files: list[Path] = []
     files: list[tuple[Path, Path]] = []
 
--- rencher/gtk/library.py
+++ rencher/gtk/library.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os.path
 import pickle
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from gi.repository import GObject
@@ -13,6 +14,7 @@ from rencher.renpy.game import GameInvalidError, GameNoExecutableError
 if TYPE_CHECKING:
     from rencher.gtk.window import RencherWindow
 
+
 class RencherLibrary(GObject.Object):
     game_items: dict[str, GameItem] = {}
     window: 'RencherWindow'
@@ -23,19 +25,20 @@ class RencherLibrary(GObject.Object):
         'game-changed': (GObject.SignalFlags.RUN_FIRST, None, (GameItem,)),
     }
 
-    def __init__(self, window):
+    def __init__(self, window: 'RencherWindow'):
         super().__init__()
         self.window = window
 
     def load_games(self) -> None:
         data_dir = RencherConfig().get_data_dir()
-        games_dir = os.path.join(data_dir, 'games')
+        games_dir = Path(data_dir) / 'games'
+        games_dir.mkdir(exist_ok=True, parents=True)
         for rpath in dict(self.game_items):
             self.remove_game(rpath)
 
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        for d in os.listdir(games_dir):
+        for d in games_dir.iterdir():
             rpath = os.path.join(games_dir, d)
             loop.run_in_executor(None, self.add_game, rpath)
 
@@ -67,7 +70,6 @@ class RencherLibrary(GObject.Object):
                 self.game_items[rpath] = game_item
                 self.emit('game-added', game_item)
                 logging.debug(f'p+{game_item.name}')
-
 
     def add_game(self, rpath: str) -> None:
         try:
